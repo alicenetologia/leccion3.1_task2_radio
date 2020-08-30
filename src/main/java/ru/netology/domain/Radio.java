013f778e5b75d2fb7b6be68a1c823b03ebca8224
Olesia D <osd0581@gmail.com>
2020-08-30 14:25:57 +0200

    modifications

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,9 +4,18 @@ public class Radio {
     private int radiostationNumberMin = 0;
     private int radiostationNumberMax = 9;
     private int currentRadiostationNumber;
-    private int volumeMax = 10;
+    private int volumeMax = 100;
     private int volumeMin = 0;
     private int currentVolume;
+    private boolean on;
+
+    public Radio(int radiostationNumberMax, int currentRadiostationNumber, int currentVolume, boolean on) {
+        this.radiostationNumberMax = radiostationNumberMax;
+        this.currentRadiostationNumber = currentRadiostationNumber;
+        this.currentVolume = currentVolume;
+        this.on = on;
+    }
+
 
     public void nextRadiostation() {
         if (currentRadiostationNumber >= radiostationNumberMin & currentRadiostationNumber < radiostationNumberMax) {
@@ -38,7 +47,6 @@ public class Radio {
 
     }
 
-
     public void volumeUp() {
         if (currentVolume >= volumeMin & currentVolume < volumeMax) {
             this.currentVolume = currentVolume + 1;
@@ -51,7 +59,6 @@ public class Radio {
         if (currentVolume < volumeMin) {
             this.currentVolume = volumeMin;
         }
-
     }
 
     public void volumeDown() {
@@ -66,7 +73,14 @@ public class Radio {
         if (currentVolume > volumeMax) {
             this.currentVolume = volumeMax;
         }
+    }
 
+    public int getRadiostationNumberMax() {
+        return radiostationNumberMax;
+    }
+
+    public void setRadiostationNumberMax(int radiostationNumberMax) {
+        this.radiostationNumberMax = radiostationNumberMax;
     }
 
     public int getCurrentRadiostationNumber() {
@@ -75,6 +89,14 @@ public class Radio {
 
     public void setCurrentRadiostationNumber(int currentRadiostationNumber) {
         this.currentRadiostationNumber = currentRadiostationNumber;
+    }
+
+    public int getVolumeMax() {
+        return volumeMax;
+    }
+
+    public void setVolumeMax(int volumeMax) {
+        this.volumeMax = volumeMax;
     }
 
     public int getCurrentVolume() {
